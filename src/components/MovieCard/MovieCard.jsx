c7a8c03041a31794c4eb41795fe74d94d7fda094
Jafar Asad <rafajdasa@gmail.com>
2023-05-28 11:40:15 -0400

    Popular Movies is working and page number rerendering

---
+++ src/components/MovieCard/MovieCard.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import StarRatings from 'react-star-ratings'
+
+function MovieCard({movie, width, height, radius, cardStyle, imgUrl, movidId}) {
+
+    const imageBaseUrl = import.meta.env.VITE_IMAGE_URL
+    const imageStyle = {
+        backgroundImage: `url(${imageBaseUrl}/${imgUrl})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        width,
+        height,
+        position: "relative",
+        borderRadius: radius,
+        boxShadow: cardStyle === "popular-card" ? "0px 0px 0px 10px 0px rgba(118 118 118 0.75)" : null
+    }
+
+    const hyperRef = `movieDetails/${movie.id}`
+
+
+  return (
+    <Link to={hyperRef} className={cardStyle}>
+        <div style={imageStyle}>
+            <div className="movie-info-top">
+                <StarRatings
+                    rating={movie?.vote_average / 2}
+                    starRatedColor="red"
+                    numberOfStars={5}
+                    starDimension='15px'
+                    starSpacing='1px'
+                 />
+            </div>
+            <div className="movie-info-bottom">
+                <p>{movie?.title}</p>
+                <p>{movie?.vote_average / 2}</p>
+            </div>
+        </div>
+        {
+            cardStyle === "top-rated-card" ? <p>{movie?.title}</p> : null
+        }
+    </Link>
+  )
+}
+
+export default MovieCard
--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react'
 import Slider from '../../components/Slider/Slider'
 import './Homepage.css'
+import PopularMovies from '../../components/PopularMovies/PopularMovies';
 import axios from 'axios';
 
 //We need to catch the apiKey and baseUrl here in the homepage. We can use props or destructure it by just writing apiKey and baseUrl
@@ -9,9 +10,9 @@ import axios from 'axios';
 
 function Homepage({apiKey, baseUrl}) {
   useEffect(()=>{
-    axios.get(`${baseUrl}/movie/popular?api_key=${apiKey}`)
-    .then((res) => console.log(res.data.results))
-    .catch(err=>console.log(err));
+    // axios.get(`${baseUrl}/movie/popular?api_key=${apiKey}`)
+    // .then((res) => console.log(res.data.results))
+    // .catch(err=>console.log(err));
     axios.get(`${baseUrl}/movie/top_rated?api_key=${apiKey}`)
     .then((res) => console.log(res.data.results))
     .catch(err=>console.log(err));
@@ -19,6 +20,9 @@ function Homepage({apiKey, baseUrl}) {
   return (
     <div className='homepage-container'>
         <Slider apiKey={apiKey} baseUrl={baseUrl} />
+        <div className="movies-wrapper">
+          <PopularMovies baseUrl={baseUrl} apiKey={apiKey} />
+        </div>
     </div>
   )
 }
