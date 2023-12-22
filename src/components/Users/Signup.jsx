7d19c848603e44f51692a1175e35651a518f3914
Jafar Asad <rafajdasa@gmail.com>
2023-12-22 21:43:29 +0300

    almost done signup page

--- src/components/Users/Signup.jsx
+++ src/components/Users/Signup.jsx
@@ -5,14 +5,28 @@ function Signup() {
   return (
     <div className='sign-container'>
         <form className='signup-form'>
-            <div className=''>
+            <div className='signup-form-header'>
                 <h1>Sign up</h1>
                 <p>Please fill in this form to create an account.</p>
             </div>
-            <div>
+            <div className='signup-form-labels'>
                 <label htmlFor="email">Email</label>
                 <input type="email" placeholder='Enter email' id='email' required />
             </div>
+            <div className='signup-form-labels'>
+                <label htmlFor="pwd">Password</label>
+                <input type="password" placeholder='Enter password' id='pwd' required />
+            </div>
+            <div className='signup-form-labels'>
+                <label htmlFor="username">Username</label>
+                <input type="text" placeholder='Enter username' id='username' required />
+            </div>
+            <div className='signup-form-button-container'>
+              <button className='cancel-btn'>Cancel</button>
+              <button className='signup-btn'>Sign up</button>
+            </div>
+
+
         </form>
     </div>
   )
